91598f5fffbffe41d483ba3d1f8ff20c2e56bea2
Dick Olsson <dick.olsson@senzilla.com>
2014-10-31 09:39:41 +0000

    Made the parsing code a bit more robust

--- src/GuzzleHttp/Message/MultipartResponse.php
+++ src/GuzzleHttp/Message/MultipartResponse.php
@@ -10,7 +10,7 @@ use GuzzleHttp\Stream\StreamInterface;
 class MultipartResponse extends Response
 {
     /** @var StreamInterface[] */
-    private $bodies;
+    private $bodies = array();
 
     /**
      * {@inheritdoc}
@@ -37,30 +37,33 @@ class MultipartResponse extends Response
     /**
      * Parses a multipart body into multiple parts.
      *
-     * @param StreamInterface $body
+     * @param StreamInterface $stream
      *
      * @return array
      */
-    public static function parseMultipartBody(StreamInterface $body)
+    public static function parseMultipartBody(StreamInterface $stream)
     {
         $parts = [];
-        preg_match('/--(.*)\b/', $body, $boundary);
-        $messages = array_filter(array_map('trim', explode($boundary[0], $body)));
+        preg_match('/--(.*)\b/', $stream, $boundary);
 
-        foreach ($messages as $message) {
-            if ($message == '--') {
-                break;
+        if (!empty($boundary)) {
+            $messages = array_filter(array_map('trim', explode($boundary[0], $stream_contents)));
+
+            foreach ($messages as $message) {
+                if ($message == '--') {
+                    break;
+                }
+                $headers = [];
+                list($header_lines, $body) = explode("\r\n\r\n", $message, 2);
+                foreach (explode("\r\n", $header_lines) as $header_line) {
+                    list($key, $value) = preg_split('/:\s+/', $header_line, 2);
+                    $headers[strtolower($key)] = $value;
+                }
+                $parts[] = [
+                    'headers' => $headers,
+                    'body' => $body,
+                ];
             }
-            $headers = [];
-            list($header_lines, $body) = explode("\r\n\r\n", $message, 2);
-            foreach (explode("\r\n", $header_lines) as $header_line) {
-                list($key, $value) = preg_split('/:\s+/', $header_line, 2);
-                $headers[strtolower($key)] = $value;
-            }
-            $parts[] = [
-                'headers' => $headers,
-                'body' => $body,
-            ];
         }
 
         return $parts;
