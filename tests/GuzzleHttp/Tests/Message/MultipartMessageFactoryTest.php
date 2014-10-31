db444f6c4b0c78db23a3279cf581d200575b6c4f
Dick Olsson <dick.olsson@senzilla.com>
2014-10-31 11:05:07 +0000

    Open a new branch with a backport for Guzzle ~4.1. Removed the server integration test because the testing API is different.

--- tests/GuzzleHttp/Tests/Message/MultipartMessageFactoryTest.php
+++ tests/GuzzleHttp/Tests/Message/MultipartMessageFactoryTest.php
@@ -2,9 +2,7 @@
 
 namespace GuzzleHttp\Tests\Message;
 
-use GuzzleHttp\Client;
 use GuzzleHttp\Message\MultipartMessageFactory;
-use GuzzleHttp\Tests\Ring\Client\Server;
 
 class MultipartMessageFactoryTest extends \PHPUnit_Framework_TestCase {
 
@@ -17,26 +15,5 @@ class MultipartMessageFactoryTest extends \PHPUnit_Framework_TestCase {
         $factory = new MultipartMessageFactory();
         $response = $factory->createResponse(200, [], 'foo');
         $this->assertInstanceOf('GuzzleHttp\Message\MultipartResponse', $response);
-    }
-
-    public function testClient()
-    {
-        $this->enqueueResponse();
-        $client = new Client(['message_factory' => new MultipartMessageFactory()]);
-        $response = $client->get(Server::$url);
-
-        $this->assertEquals('hello', (string) $response->getBody());
-        $this->assertEquals('hejsan', (string) $response->getBody());
-    }
-
-    protected function enqueueResponse()
-    {
-        Server::flush();
-        $response = [
-            'status' => 200,
-            'headers' => ['Content-Type' => 'multipart/related; boundary="delimiter"'],
-            'body' => "--delimiter\r\nLanguage: en\r\n\r\nhello\r\n--delimiter\r\nLanguage: se\r\n\r\nhejsan\r\n--delimiter--",
-        ];
-        Server::enqueue([$response]);
     }
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,2 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/guzzlehttp/ringphp/tests/Client/Server.php';
-
-use GuzzleHttp\Tests\Ring\Client\Server;
-
-Server::start();
-
-register_shutdown_function(function () {
-    Server::stop();
-});
